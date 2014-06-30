ade687a54ac0fe9c01129f8353b8ce19ab8bc77f
Anurag Meena <ianuragit@gmail.com>
2014-06-30 23:53:02 +0530

    preliminary code -1

--- vote_trest.php
+++ vote_trest.php
@@ -25,11 +25,6 @@ $array[5] = $shiladitya;
 $array[6] = $tushar;
 
 
-$array = array("foo", "bar", "hello", "world");
-$conn=mysql_connect('localhost', 'mysql_user', 'mysql_password');
-mysql_select_db("mysql_db",$conn);
-$array_string=mysql_escape_string(serialize($array));
-mysql_query("insert into table (column) values($array_string)",$conn);
 
 
 
@@ -51,41 +46,4 @@ mysql_close($bd);
 
 
 
-
-if ($vote == 0) {
-  $yes = $yes + 1;
-}
-if ($vote == 1) {
-  $no = $no + 1;
-}
-
-//inserting votes to txt file
-$insertvote = $aman."||".$anurag."||".$aritra."||".$arpan."||".$saurabh."||".$shiladitya."||".$tushar;
-$fp = fopen($filename,"w");
-fputs($fp,$insertvote);
-fclose($fp);
-?>
-
-
-
-<h2>Result:</h2>
-<table>
-<tr>
-width='<?php echo(100*round($yes/($no+$yes),2)); 
-<td>Yes:</td>
-<td>
-<img src="poll.gif"?>'
-height='20'>
-<?php echo(100*round($yes/($no+$yes),2)); ?>%
-</td>
-</tr>
-<tr>
-<td>No:</td>
-<td>
-<img src="poll.gif"
-width='<?php echo(100*round($no/($no+$yes),2)); ?>'
-height='20'>
-<?php echo(100*round($no/($no+$yes),2)); ?>%
-</td>
-</tr>
 </table>
